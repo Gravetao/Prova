31faabe0c2eacdd6c670c00cca7e2089c33d3533
carolmoises <carol.moises@gmail.com>
2021-05-11 22:22:32 -0300

    Alterado a view do helloworld com a indexteste.html

--- helloworldapp/views.py
+++ helloworldapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def helloworld_views(request):
-    return render(request, 'helloworldapp/index.html')
+    return render(request, 'helloworldapp/indexteste.html')
